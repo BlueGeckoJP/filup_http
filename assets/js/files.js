8bd0b3c1aef34982ca4a928c7b19d553de0ab7e5
BlueGecko <kei09doya@gmail.com>
2024-07-08 08:57:51 +0900

    redesigned files page

--- assets/js/files.js
+++ assets/js/files.js
@@ -1,5 +1,6 @@
-function onClickRemove(filename) {
+function onClickRemove() {
 	if (confirm("Are you sure you want to delete the file?")) {
+		const filename = document.getElementById("filename").innerHTML;
 		fetch("/api/remove", {
 			method: "POST",
 			body: filename,
@@ -7,7 +8,7 @@ function onClickRemove(filename) {
 			if (e.status === 200) {
 				alert("OK");
 			} else {
-				alert("Status code was not normal | " + e.status);
+				alert("Status code was not normal: " + e.status);
 			}
 		});
 	} else {
@@ -15,4 +16,27 @@ function onClickRemove(filename) {
 	}
 
 	document.location.reload();
+}
+
+function onClickDownload(filepath) {
+	let element = document.createElement("a");
+	element.href = filepath;
+	element.download = "";
+	element.click();
+}
+
+function onDOMLoaded() {
+	let item_list = document.getElementById("item-list");
+	if (item_list.children.length === 0) {
+		let element = document.createElement("p");
+		element.innerHTML = "ファイルはありません";
+		element.id = "not-found-msg";
+		item_list.appendChild(element);
+	}
+}
+
+if (document.readyState === "loading") {
+	document.addEventListener("DOMContentLoaded", onDOMLoaded);
+} else {
+	onDOMLoaded();
 }
